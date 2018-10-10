2cae711c98f31827afeda361922db73b87916838
SooSoo-El-Shreer <smb26@mail.aub.edu>
2018-10-10 10:30:55 +0300

    2048 & Sudoku Vue components added

---
+++ Frontend/gamehub/src/components/twenty48.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    	msg: 'Welcome to Your Sudoku Game',
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
