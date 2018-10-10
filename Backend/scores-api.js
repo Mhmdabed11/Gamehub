ceeacf8c9e4ea95a9fa8c4346173d73d6b4a3e92
achahine78 <achahine78@gmail.com>
2018-10-10 13:22:02 +0300

    finished scores api

---
+++ Backend/scores-api.js
@@ -0,0 +1,54 @@
+var errors = require('restify-errors');
+var nano = require('nano')('http://localhost:5984/gamehub-db');
+
+module.exports = {
+    saveScore: function(req, res, next){
+        var username = req.body.username;
+        var game = req.body.game;
+        var score = req.body.score;
+
+        var newGame = {
+            username: username,
+            game: game,
+            score: score,
+            type: "score"
+        }
+
+        nano.insert(newGame).then((response) => {
+            res.send(response);
+            return next();
+        })
+    },
+    getByUsername: function(req, res, next){
+        var username = req.params.username;
+
+        nano.view('scores', 'getByUsername', {
+            'key': username
+        }).then((body) => {
+            //console.log(body.rows[0].value)
+            if(body.rows.length > 0){
+                res.send(body.rows[0].value)
+                return next();
+            } else {
+                req.userFound = false;
+                return next(new errors.NotFoundError("User not found!"));
+            }
+        });
+    },
+    getByGame: function(req, res, next){
+        var game = req.params.game;
+
+        nano.view('scores', 'getByGame', {
+            'key': game
+        }).then((body) => {
+            //console.log(body.rows[0].value)
+            if(body.rows.length > 0){
+                res.send(body.rows[0].value)
+                return next();
+            } else {
+                req.userFound = false;
+                return next(new errors.NotFoundError("Game not found!"));
+            }
+        });  
+    }
+}
--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,6 @@
 var restify = require('restify');
 var userApi = require('./user-api');
+var scoresApi = require('./scores-api')
 
 var server = restify.createServer();
 
@@ -8,6 +9,9 @@ server.use(restify.plugins.queryParser());
 
 server.post('/login', userApi.checkUserExists, userApi.checkCorrectPassword);
 server.post('/signup', userApi.checkUserExists, userApi.addUser);
+server.post('/saveScore', scoresApi.saveScore);
+server.get('/getScoresByUsername/:username', scoresApi.getByUsername);
+server.get('/getScoresByGame/:game', scoresApi.getByGame);
 
 server.listen(8888, function() {
     console.log('%s listening at %s', server.name, server.url);
