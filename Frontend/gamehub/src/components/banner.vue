d489333b93dfde788cfc56e224ab1d0046a63143
Mhmd Abed <mabed4297@gmail.com>
2018-10-11 15:21:58 +0300

    added username to store.js

--- Frontend/gamehub/src/components/banner.vue
+++ Frontend/gamehub/src/components/banner.vue
@@ -31,7 +31,7 @@
 
     <button v-on:click="reversi" class="menubutton"> Reversi  </button>
     <button v-on:click="sudoku" class="menubutton"> Sudoku </button>
-    
+    <p>{{this.$store.getters.getusername}}</p>
 <label>Username:</label>
 <input type="txt" placesholder="Username">
 <label>Password:</label>
@@ -43,71 +43,99 @@
 </template>
 
  <script>
- export default {
-
-
+export default {
   data() {
-  return{
-        };
-},
-methods:{
-    twozero:function(){
-this.$router.push("/twenty48")
-
+    return {};
+  },
+  methods: {
+    twozero: function() {
+      this.$router.push("/twenty48");
     },
-    hangman:function(){
-this.$router.push("/hangman")
-
+    hangman: function() {
+      this.$router.push("/hangman");
     },
-     reversi:function(){
-this.$router.push("/reversi")
-
+    reversi: function() {
+      this.$router.push("/reversi");
     },
-     memory:function(){
-this.$router.push("/memory")
-
+    memory: function() {
+      this.$router.push("/memory");
     },
-     checkers:function(){
-this.$router.push("/checkers")
-
+    checkers: function() {
+      this.$router.push("/checkers");
     },
-     puzzle:function(){
-this.$router.push("/puzzleslider")
-
+    puzzle: function() {
+      this.$router.push("/puzzleslider");
     },
-     sudoku:function(){
-this.$router.push("/sudoku")
-
+    sudoku: function() {
+      this.$router.push("/sudoku");
     },
-     connect:function(){
-this.$router.push("/connect4")
-
+    connect: function() {
+      this.$router.push("/connect4");
     },
-     mines:function(){
-this.$router.push("/minesweeper")
-
-    },
-
-}
-}
+    mines: function() {
+      this.$router.push("/minesweeper");
+    }
+  }
+};
 </script>
 
 <style scoped>
-
-#ban{width:100%;height:75px;background-color:black;display:flex;flex-direction: row;justify-content:space-between;margin:0px;}
-.menubutton{height:50%;margin-top:18px;}
-input{height:30%;margin-top:20px;}
-label{margin-top:30px;color:white;}
-@media screen and (max-width:800px){
-.menubutton{display:none;}
-.dropmenu{display:block !important;margin:0px;}
-
+#ban {
+  width: 100%;
+  height: 75px;
+  background-color: black;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin: 0px;
 }
-.dropmenu{display:none;margin:0px;}
-.dropmenu:hover{position:relative;}
-.menu{display:none;position:absolute;background-color:black;}
-.dropmenu:hover  .menu{display:flex;flex-direction: column;justify-content:space-between;}
-.menu:hover{display:flex;flex-direction: column;justify-content:space-between;}
-.dropbutton{height:50px;}
-img{margin:0px;}
+.menubutton {
+  height: 50%;
+  margin-top: 18px;
+}
+input {
+  height: 30%;
+  margin-top: 20px;
+}
+label {
+  margin-top: 30px;
+  color: white;
+}
+@media screen and (max-width: 800px) {
+  .menubutton {
+    display: none;
+  }
+  .dropmenu {
+    display: block !important;
+    margin: 0px;
+  }
+}
+.dropmenu {
+  display: none;
+  margin: 0px;
+}
+.dropmenu:hover {
+  position: relative;
+}
+.menu {
+  display: none;
+  position: absolute;
+  background-color: black;
+}
+.dropmenu:hover .menu {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+.menu:hover {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+.dropbutton {
+  height: 50px;
+}
+img {
+  margin: 0px;
+}
 </style>
