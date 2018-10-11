def5bd9a596310c0c6764c42c072fc4322d10e53
achahine78 <achahine78@gmail.com>
2018-10-11 12:08:57 +0300

    might have fixed cors

--- Backend/server.js
+++ Backend/server.js
@@ -2,10 +2,21 @@ var restify = require('restify');
 var userApi = require('./user-api');
 var scoresApi = require('./scores-api')
 
+const corsMiddleware = require('restify-cors-middleware')
+
 var server = restify.createServer();
 
 server.use(restify.plugins.bodyParser());
 server.use(restify.plugins.queryParser());
+
+const cors = corsMiddleware({
+    //preflightMaxAge: 5, //Optional
+    origins: ['*'],
+    allowHeaders: ['*']
+  })
+   
+  server.pre(cors.preflight)
+  server.use(cors.actual)
 
 server.post('/login', userApi.checkUserExists, userApi.checkCorrectPassword);
 server.post('/signup', userApi.checkUserExists, userApi.addUser);
