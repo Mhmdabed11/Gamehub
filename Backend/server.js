254ed34b2f3de7c8ed7f00b16c3458c6a8353e2a
Mhmd Abed <mabed4297@gmail.com>
2018-10-11 11:00:36 +0300

    deleted CORS dependency in server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,18 +1,11 @@
 var restify = require('restify');
 var userApi = require('./user-api');
 var scoresApi = require('./scores-api')
-const corsMiddleware = require('restify-cors-middleware')
 
 var server = restify.createServer();
 
 server.use(restify.plugins.bodyParser());
 server.use(restify.plugins.queryParser());
-server.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', "*");
-    res.header('Access-Control-Allow-Methods', "GET,PUT,POST,DELETE,OPTIONS");
-    res.header('Access-Control-Allow-Headers', "Content-Type");
-    next();
-})
 
 server.post('/login', userApi.checkUserExists, userApi.checkCorrectPassword);
 server.post('/signup', userApi.checkUserExists, userApi.addUser);
