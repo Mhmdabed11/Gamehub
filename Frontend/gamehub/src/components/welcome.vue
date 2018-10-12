b8075c31befa6a4fbe1a83adf6898e58009d059b
Mhmd Abed <mabed4297@gmail.com>
2018-10-12 13:01:43 +0300

    removed image from assets

--- Frontend/gamehub/src/components/welcome.vue
+++ Frontend/gamehub/src/components/welcome.vue
@@ -1,18 +1,15 @@
 <template>
     <div>
         <h1>Welcome to Gamehub, Please select a game from above and beat your Highscore.</h1>
-        <img :src="image" >
+    
     </div>
 </template>
 
 <script>
-import image from "/Users/mohammadabed/gamehub/Gamehub/Frontend/gamehub/src/assets/liamneeson.jpg";
 export default {
   name: "welcome",
   data() {
-    return {
-      image: image
-    };
+    return {};
   },
   methods: {}
 };
